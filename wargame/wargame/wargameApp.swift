ca3abeef6adcbce99ba8e4d89182f1a67eebff8e
Luca Galati <47124644+TheGalatiator@users.noreply.github.com>
2021-08-03 19:26:20 -0400

    Add files via upload

---
+++ wargame/wargame/wargameApp.swift
@@ -0,0 +1,17 @@
+//
+//  wargameApp.swift
+//  wargame
+//
+//  Created by Luca Galati on 2021-06-26.
+//
+
+import SwiftUI
+
+@main
+struct wargameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
